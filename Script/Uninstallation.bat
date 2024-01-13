a23ef232850f14727f6dbad82f043caf41018ba4
SstealzZ <antoine.veber@epitech.eu>
2024-01-13 10:27:07 +0000

    Add Uninstallation.bat

---
+++ Script/Uninstallation.bat
@@ -0,0 +1,16 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+
+:: Set the registry path
+set KEY_PATH="HKEY_CLASSES_ROOT\*\shell\SpeedLocker\command"
+
+:: Delete the registry key
+reg delete %KEY_PATH% /f
+
+if %ERRORLEVEL% == 0 (
+    echo Registry key deleted successfully!
+) else (
+    echo Failed to delete registry key. Please ensure you have administrative rights.
+)
+
+exit
